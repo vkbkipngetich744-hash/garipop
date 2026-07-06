a0a16c13bf1eca9ec195ec801e1774d8d93bdb32
vkbkipngetich744-hash <vkbkipngetich744@gmail.com>
2026-07-06 15:16:08 +0300

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#111827",
+        color: "white",
+        textAlign: "center",
+        padding: "40px 20px"
+      }}
+    >
+      <h2>🚗 Garipop</h2>
+
+      <p style={{ marginTop: "10px" }}>
+        Kenya's Smart Car Marketplace
+      </p>
+
+      <p style={{ marginTop: "20px", color: "#9ca3af" }}>
+        © 2026 Garipop. All Rights Reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
